df07f4e044d980e9e25391e633ad684c18faf576
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-03 16:16:28 +0600

    Updates

---
+++ precache-manifest.87c5a9b4823f55a5e594313a0a544b19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e179368be42caaecdfdf526aaa731b",
+    "url": "/vending-machine/index.html"
+  },
+  {
+    "revision": "13dd4c81930b7579b173",
+    "url": "/vending-machine/static/css/main.dae79e9f.chunk.css"
+  },
+  {
+    "revision": "77ff94b709328c93f7fc",
+    "url": "/vending-machine/static/js/2.238ab2f2.chunk.js"
+  },
+  {
+    "revision": "13dd4c81930b7579b173",
+    "url": "/vending-machine/static/js/main.3e3cffbf.chunk.js"
+  },
+  {
+    "revision": "1f3225add9a7fca75947",
+    "url": "/vending-machine/static/js/runtime-main.2434a4c0.js"
+  },
+  {
+    "revision": "0e21446e438c1aeb2913a4c95776436a",
+    "url": "/vending-machine/static/media/Chips.0e21446e.png"
+  },
+  {
+    "revision": "7746d7d5d94a644ffd6cdb1876f72a87",
+    "url": "/vending-machine/static/media/Soda.7746d7d5.png"
+  },
+  {
+    "revision": "369bea35cf50c57aa4cb31df6c8f4da3",
+    "url": "/vending-machine/static/media/VendingMachine.369bea35.png"
+  }
+]);
